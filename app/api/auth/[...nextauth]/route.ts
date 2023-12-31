e9c1da915ed83e22ab036916c1a9c4b900ff735a
ashish0083 <kondapallyashish@gmail.com>
2023-12-31 21:43:46 +1100

    add prisma models

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,22 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import {Prisma} from "@prisma/client"
+import {PrismaClient} from "@prisma/client"
 import { Adapter } from "next-auth/adapters";
 
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(Prisma) as Adapter,
+    adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID!,
             clientSecret: process.env.GITHUB_CLIENT_SECRET!
         })
-    ]
+    ],
+    session: {
+        strategy: 'jwt'
+    }
 }
 
 const handler = NextAuth(authOptions)
