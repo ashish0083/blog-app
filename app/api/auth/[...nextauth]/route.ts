5b4ed393c30d01f6aa6b1bb693e2f4668b7afad2
ashish0083 <kondapallyashish@gmail.com>
2024-01-04 13:39:39 +1100

    Credentials Provider update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,8 @@ import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/db";
 import { Adapter } from "next-auth/adapters";
+import CredentialsProvider from "next-auth/providers/credentials";
+
 
 
 export const authOptions: NextAuthOptions = {
@@ -11,6 +13,21 @@ export const authOptions: NextAuthOptions = {
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID!,
             clientSecret: process.env.GITHUB_CLIENT_SECRET!
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "Type your username" },
+                password: { label: "Password", type: "password" },
+            },
+            authorize(credentials, req) {
+                const user = { id: "1", name: "Ash K", email: "ashk@example.com", password: "123456" }
+                if(user) {
+                    return user; 
+                } else {
+                    return null;
+                }
+            }
         })
     ],
     session: {
